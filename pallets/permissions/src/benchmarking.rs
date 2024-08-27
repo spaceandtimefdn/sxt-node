e0d8bbf85e5799240f64a5e052d4314d55e33709
zachary frederick <zachfred62@gmail.com>
2024-08-27 13:02:11 -0400

    feat:skeleton permissions pallet

---
+++ pallets/permissions/src/benchmarking.rs
@@ -0,0 +1,12 @@
+#![cfg(feature = "runtime-benchmarks")]
+use super::*;
+
+#[allow(unused)]
+use crate::Pallet as Permissions;
+use frame_benchmarking::v2::*;
+use frame_system::RawOrigin;
+
+#[benchmarks]
+mod benchmarks {
+    use super::*;
+}
