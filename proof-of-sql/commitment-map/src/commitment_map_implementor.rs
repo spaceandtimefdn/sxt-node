ab988dd40b3ebc175d5d3f224d6833823acd3458
Trevor Lovell <trevor.lovell@spaceandtime.io>
2024-09-03 20:34:32 -0600

    feat: add CommitmentMap abstraction with test implementation
    As we develop the substrate "commitment pallet", we will need to store commitments or hashes of commitments on chain so that they go through consensus, and are ultimately signed.
    The substrate storage abstractions essentially provide a key-value store to accomplish storage operations like this.
    In our case, the key will be (schema_name, table_name, commitment_scheme) and the value will be the commitment, or a hash of the commitment.
    
    This PR provides an abstraction for representing mappings of this kind.
    It implements many of the proof-of-sql-specific considerations.
    In particular
    - we want commitment scheme usage to be extensible to add more commitment schemes in the future
    - we want to update all commitment schemes defined for a table simultaneously
    - we don't want to update a table with commitment schemes that it was not originally defined with
    
    This abstraction is tested by the test implementation MemoryCommitmentMap.

---
+++ proof-of-sql/commitment-map/src/commitment_map_implementor.rs
@@ -0,0 +1,90 @@
+use crate::{
+    generic_over_commitment::GenericOverCommitment, AnyCommitmentScheme, CommitmentMap,
+    CommitmentScheme, CommitmentSchemeFlags, CommitmentSchemesMismatchError, KeyExistsError,
+    PerCommitmentScheme,
+};
+use core::fmt::Debug;
+
+/// Private abstraction for mappings of keys and commitment schemes to values of type `V`.
+///
+/// This trait allows for easy implementation of [`CommitmentMap`].
+/// Intended for maps that don't have optimizations around setting multiple values simultaneously.
+///
+/// This trait should remain crate-private.
+/// Only the key-atomic [`CommitmentMap`] operations should be available outside of the crate.
+pub trait CommitmentMapImplementor<K, V: GenericOverCommitment> {
+    /// Returns `true` if this map contains this key and scheme.
+    fn has_key_and_scheme_impl(&self, key: &K, scheme: &CommitmentScheme) -> bool;
+
+    /// Set the commitment in this map for a particular key and any scheme.
+    fn set_commitment_for_any_scheme_impl(&mut self, key: K, commitment: AnyCommitmentScheme<V>);
+
+    /// Delete commitment for a particular key and any scheme.
+    fn delete_commitment_for_any_scheme_impl(
+        &mut self,
+        key: &K,
+        commitment_scheme: &CommitmentScheme,
+    );
+}
+
+impl<M: CommitmentMapImplementor<K, V>, K: Clone + Debug, V: GenericOverCommitment>
+    CommitmentMap<K, V> for M
+{
+    fn has_key_and_scheme(&self, key: &K, scheme: &CommitmentScheme) -> bool {
+        self.has_key_and_scheme_impl(key, scheme)
+    }
+
+    fn schemes_for_key(&self, key: &K) -> CommitmentSchemeFlags {
+        CommitmentSchemeFlags::all()
+            .into_iter()
+            .filter(|scheme| self.has_key_and_scheme_impl(key, scheme))
+            .collect()
+    }
+
+    fn has_key(&self, key: &K) -> bool {
+        self.schemes_for_key(key).into_iter().count() > 0
+    }
+
+    fn update_commitments(
+        &mut self,
+        key: K,
+        commitments: PerCommitmentScheme<V>,
+    ) -> Result<(), CommitmentSchemesMismatchError> {
+        let original_schemes = self.schemes_for_key(&key);
+
+        let new_schemes = commitments.to_flags();
+
+        if original_schemes != new_schemes {
+            return Err(CommitmentSchemesMismatchError {
+                original_schemes,
+                new_schemes,
+            });
+        }
+
+        commitments
+            .into_iter()
+            .for_each(|c| self.set_commitment_for_any_scheme_impl(key.clone(), c));
+        Ok(())
+    }
+
+    fn create_commitments(
+        &mut self,
+        key: K,
+        commitments: PerCommitmentScheme<V>,
+    ) -> Result<(), KeyExistsError<K>> {
+        if self.has_key(&key) {
+            return Err(KeyExistsError { key });
+        }
+
+        commitments
+            .into_iter()
+            .for_each(|c| self.set_commitment_for_any_scheme_impl(key.clone(), c));
+        Ok(())
+    }
+
+    fn delete_commitments(&mut self, key: &K) {
+        CommitmentSchemeFlags::all()
+            .into_iter()
+            .for_each(|c| self.delete_commitment_for_any_scheme_impl(key, &c));
+    }
+}
