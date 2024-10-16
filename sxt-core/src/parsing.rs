713630920b45c04597cf0d17f6f420d9fc170340
Trevor Lovell <trevor.lovell@spaceandtime.io>
2024-10-15 18:49:48 -0600

    fix: depend on workspace sqlparser definition in sxt-core
    This repository has conflicting sqlparser dependencies.
    The workspace uses version 0.45, and sxt-core uses version 0.51.
    This discrepancy needs to be addressed before sxt-core code that depends
    on sqlparser is used in the pallets.

--- sxt-core/src/parsing.rs
+++ sxt-core/src/parsing.rs
@@ -1,10 +1,10 @@
-use std::{fs::read_to_string, path::Path};
-
-use sqlparser::{ast::Statement, dialect::GenericDialect, parser::Parser};
-
 use crate::tables::{
     create_statement, table_identifier, CreateStatement, SourceAndMode, TableIdentifier,
 };
+use sqlparser::{
+    ast::helpers::stmt_create_table::CreateTableBuilder, dialect::GenericDialect, parser::Parser,
+};
+use std::{fs::read_to_string, path::Path};
 
 /// Read a ddl file from a path and parse it into create table statements, any error will cause a panic
 pub fn ddl_to_tables(
@@ -19,19 +19,19 @@ pub fn ddl_to_tables(
     let statements = parser.parse_statements().unwrap();
 
     statements
-        .iter()
-        .filter_map(|x| match x {
-            Statement::CreateTable(c) => {
+        .into_iter()
+        .filter_map(|x| match CreateTableBuilder::try_from(x) {
+            Ok(c) => {
                 let name = c.name.to_string();
                 let pieces: Vec<&str> = name.split(".").collect();
                 let namespace = pieces.first().unwrap();
                 let name = pieces.get(1).unwrap();
-                let s = c.to_string();
+                let s = c.build().to_string();
                 let sm = sm.clone();
 
                 Some((sm, table_identifier(name, namespace), create_statement(&s)))
             }
-            _ => None,
+            Err(_) => None,
         })
         .collect()
 }
@@ -57,11 +57,9 @@ mod tests {
 
     #[test]
     fn ddls_to_genesis_works() {
-        ddls_to_genesis(
-            vec![
-                ("testing/ddl.sql".into(), SourceAndMode::default()),
-                ("testing/ddl2.sql".into(), SourceAndMode::default())
-            ]
-        );
+        ddls_to_genesis(vec![
+            ("testing/ddl.sql".into(), SourceAndMode::default()),
+            ("testing/ddl2.sql".into(), SourceAndMode::default()),
+        ]);
     }
 }
