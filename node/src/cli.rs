455025098e9b0bd46b92a015f05749caae0555fb
zachary frederick <zachfred62@gmail.com>
2024-10-26 12:02:04 -0400

    feat: add --with-db flag to enable acting as a prover

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -7,6 +7,10 @@ pub struct Cli {
 
     #[clap(flatten)]
     pub run: RunCmd,
+
+    /// Start the node with an associated SQL database to act as a Prover for the network
+    #[clap(long)]
+    pub with_db: bool,
 }
 
 #[derive(Debug, clap::Subcommand)]
