6a8b46442969ad3b84264b9e64b7c5adf2c2a321
Trevor Lovell <trevor.lovell@spaceandtime.io>
2025-01-13 16:34:45 -0700

    feat: wrap process_insert native interface in NativeApi method
    Commitment computation in the runtime is too heavy, so a native
    interface has been provided for it. In order for this to be used within
    pallets, it needs to be exposed via the NativeApi trait. This change
    adds a NativeApi::process_insert method accordingly.

--- native-api/src/lib.rs
+++ native-api/src/lib.rs
@@ -1,8 +1,15 @@
 //! The declaration for the native trait that can be adhered to by pallets that want to execute std code.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use sxt_core::native::{CreateStatementPassBy, NativeError, OnChainTableBytes, RowData};
-use sxt_core::tables::CreateStatement;
+use proof_of_sql_commitment_map::TableCommitmentBytesPerCommitmentSchemePassBy;
+use sxt_core::native::{
+    CreateStatementPassBy,
+    NativeCommitmentError,
+    NativeError,
+    OnChainTableBytes,
+    RowData,
+};
+use sxt_core::tables::TableIdentifier;
 
 /// The native api that our pallets can adhere to.
 /// The inputs and output to these types need to implement the `PassByCode` trait.
@@ -12,14 +19,45 @@ pub trait NativeApi: 'static {
         row_data: RowData,
         create_statement: CreateStatementPassBy,
     ) -> Result<OnChainTableBytes, NativeError>;
+
+    /// Process insert to support commitment metadata.
+    ///
+    /// Returns..
+    /// - the processed insert data with comitment metadata
+    /// - the updated commitments for the table
+    fn process_insert(
+        table_identifier: TableIdentifier,
+        insert_data_bytes: OnChainTableBytes,
+        previous_commitments_bytes: TableCommitmentBytesPerCommitmentSchemePassBy,
+    ) -> Result<
+        (
+            OnChainTableBytes,
+            TableCommitmentBytesPerCommitmentSchemePassBy,
+        ),
+        NativeCommitmentError,
+    >;
 }
 
 /// Needed for type checks in pallets, if adding new functions to the NativeApi they will need to be implemented here.
 impl NativeApi for () {
     fn record_batch_to_onchain(
-        row_data: RowData,
-        create_statement: CreateStatementPassBy,
+        _row_data: RowData,
+        _create_statement: CreateStatementPassBy,
     ) -> Result<OnChainTableBytes, NativeError> {
+        unimplemented!()
+    }
+
+    fn process_insert(
+        _table_identifier: TableIdentifier,
+        _insert_data_bytes: OnChainTableBytes,
+        _previous_commitments_bytes: TableCommitmentBytesPerCommitmentSchemePassBy,
+    ) -> Result<
+        (
+            OnChainTableBytes,
+            TableCommitmentBytesPerCommitmentSchemePassBy,
+        ),
+        NativeCommitmentError,
+    > {
         unimplemented!()
     }
 }
@@ -33,5 +71,23 @@ impl NativeApi for Api {
         create_statement: CreateStatementPassBy,
     ) -> Result<OnChainTableBytes, NativeError> {
         native::interface::record_batch_to_onchain(row_data, create_statement)
+    }
+
+    fn process_insert(
+        table_identifier: TableIdentifier,
+        insert_data_bytes: OnChainTableBytes,
+        previous_commitments_bytes: TableCommitmentBytesPerCommitmentSchemePassBy,
+    ) -> Result<
+        (
+            OnChainTableBytes,
+            TableCommitmentBytesPerCommitmentSchemePassBy,
+        ),
+        NativeCommitmentError,
+    > {
+        native::interface::process_insert(
+            table_identifier,
+            insert_data_bytes,
+            previous_commitments_bytes,
+        )
     }
 }
