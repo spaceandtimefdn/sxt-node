9f7c19a8918a12f3017563823c3d82bda1b1c25b
Trevor Lovell <trevor.lovell@spaceandtime.io>
2025-02-21 13:55:53 -0700

    feat: feature-flag multiple static-setups nu sizes
    After switching to native commitment generation, the only commitment
    operations being performed in the runtime are commitments to empty
    tables (to initialize commitments for new tables). This means that the
    runtime doesn't need a very large setup to perform all of its commitment
    computation. This change makes it so a tiny setup with nu 1 is used in
    the runtime while the current setup with nu 14 is used in native.

--- proof-of-sql/static-setups/src/lib.rs
+++ proof-of-sql/static-setups/src/lib.rs
@@ -7,9 +7,16 @@ use proof_of_sql_commitment_map::generic_over_commitment::AssociatedPublicSetupT
 use proof_of_sql_commitment_map::PerCommitmentScheme;
 
 /// Ark-serialized bytes of proof-of-sql public parameters, generated with...
-/// - max_nu of 16
+/// - nu of 1
 /// - ChaCha20Rng with seed "SpaceAndTime"
-const PUBLIC_PARAMETERS_BYTES: &[u8; 4719080] = include_bytes!("../public_parameters");
+#[cfg(all(feature = "nu_1", not(feature = "nu_14")))]
+const PUBLIC_PARAMETERS_BYTES: &[u8; 1064] = include_bytes!("../public_parameters_nu_1");
+
+/// Ark-serialized bytes of proof-of-sql public parameters, generated with...
+/// - nu of 14
+/// - ChaCha20Rng with seed "SpaceAndTime"
+#[cfg(feature = "nu_14")]
+const PUBLIC_PARAMETERS_BYTES: &[u8; 4719080] = include_bytes!("../public_parameters_nu_14");
 
 lazy_static::lazy_static! {
     /// Proof-of-sql PublicParameters, built from [`PUBLIC_PARAMETERS_BYTES`].
