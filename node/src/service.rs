f6dde51c1a022be89c5586d53d8c1f0f1f1ac64e
Autocoder <autocoder@example.com>
2025-03-09 14:59:44 -0400

    fix: appease clippy

--- node/src/service.rs
+++ node/src/service.rs
@@ -560,14 +560,12 @@ pub fn new_full(config: Configuration, cli: Cli) -> Result<TaskManager, ServiceE
 
     let task_manager = match config.network.network_backend {
         sc_network::config::NetworkBackendType::Libp2p => {
-            let task_manager = new_full_base::<sc_network::NetworkWorker<_, _>>(config, with_db)
-                .map(|NewFullBase { task_manager, .. }| task_manager)?;
-            task_manager
+            new_full_base::<sc_network::NetworkWorker<_, _>>(config, with_db)
+                .map(|NewFullBase { task_manager, .. }| task_manager)?
         }
         sc_network::config::NetworkBackendType::Litep2p => {
-            let task_manager = new_full_base::<sc_network::Litep2pNetworkBackend>(config, with_db)
-                .map(|NewFullBase { task_manager, .. }| task_manager)?;
-            task_manager
+            new_full_base::<sc_network::Litep2pNetworkBackend>(config, with_db)
+                .map(|NewFullBase { task_manager, .. }| task_manager)?
         }
     };
 
