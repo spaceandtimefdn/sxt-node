2e033346d701a6a05a7370a0f2d708f43ceab559
Trevor Lovell <trevor.lovell@spaceandtime.io>
2025-03-12 18:18:06 -0600

    feat: store loaded proof-of-sql setups statically
    Proof-of-sql setups are currently baked into the native and runtime
    binaries. This is not strictly necessary for the native code, as native
    code has access to io. This will be especially important as we add
    hyperkzg support, since hyperkzg setups can be quite large.
    
    This defines static OnceCells that loaded proof-of-sql setups will be
    stored in. Native interfaces cannot access values created outside of the
    interface unless they are simply static. This also adds functions for
    initializing these cells from configuration.

--- proof-of-sql/static-setups/src/io/mod.rs
+++ proof-of-sql/static-setups/src/io/mod.rs
@@ -1,3 +1,12 @@
 //! Contains static public setups that can be initialized with io operations.
 mod args;
 pub use args::{LoadPublicSetupError, ProofOfSqlPublicSetupArgs};
+
+mod cells;
+pub use cells::{
+    initialize_from_config,
+    initialize_from_file_unchecked,
+    InitializePublicSetupError,
+    PublicSetupAlreadyInitialized,
+    PUBLIC_SETUPS,
+};
