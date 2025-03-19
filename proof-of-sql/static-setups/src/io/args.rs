812e8aa1e437d4eb75ef6688520f91bb3d677f0b
Trevor Lovell <trevor.lovell@spaceandtime.io>
2025-03-18 23:58:47 -0600

    feat: download hyperkzg public setup files
    We want to be able to download hyperkzg public setup files on startup.
    This adds a function for doing so that accepts a destination directory
    for the public setup files to be downloaded to, and a "release degree"
    parameter that corresponds to a proof-of-sql github release of
    compressed ppot files. If the files that would be downloaded from the
    github release already exist, they are skipped.

--- proof-of-sql/static-setups/src/io/args.rs
+++ proof-of-sql/static-setups/src/io/args.rs
@@ -1,13 +1,20 @@
 use core::future::Future;
 use core::pin::Pin;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use ark_serialize::{CanonicalDeserialize, Compress, Validate};
 use clap::Args;
+use futures::future::try_join_all;
+use futures::lock::Mutex;
+use futures::stream::StreamExt;
+use futures::TryStreamExt;
 use hex::FromHex;
 use proof_of_sql::proof_primitive::dory::PublicParameters;
+use reqwest::Response;
 use sha2::{Digest, Sha256};
 use snafu::Snafu;
+use tokio::io::AsyncWriteExt;
 use url::Url;
 
 /// CLI args for loading proof-of-sql public setups.
@@ -48,6 +55,12 @@ pub enum LoadPublicSetupError {
     Io {
         /// The source io error.
         source: std::io::Error,
+    },
+    /// Failed to parallelize task.
+    #[snafu(display("failed to parallelize task: {source}"), context(false))]
+    Threading {
+        /// The source tokio error.
+        source: tokio::task::JoinError,
     },
     /// Failed to verify setup against sha256sum.
     #[snafu(display("failed to verify setup against sha256sum"))]
@@ -103,12 +116,126 @@ pub async fn load_dory_public_setup(
     )?)
 }
 
+const PROOF_OF_SQL_RELEASE_DOWNLOADS_URL: &str =
+    "https://github.com/spaceandtimelabs/sxt-proof-of-sql/releases/download/";
+
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+
+/// Doing 2 tasks per cpu ensures that the temporary memory cost of deserialization/decompression
+/// never exceeds 125% of the ultimate decompressed setup.
+const DESERIALIZATION_TASKS_PER_CPU: usize = 2;
+
+/// Download hyperkzg public setup files to the given directory if they do not exist.
+async fn download_hyperkzg_public_setup_files(
+    directory: &Path,
+    release_degree: &str,
+) -> Result<Vec<PathBuf>, LoadPublicSetupError> {
+    let release_downloads_url: Url = format!(
+        "{PROOF_OF_SQL_RELEASE_DOWNLOADS_URL}ppot_0080_{release_degree}_compressed_elements/",
+    )
+    .parse()
+    .unwrap();
+
+    let http_client = reqwest::Client::new();
+
+    let single_file_name = format!("ppot_0080_{release_degree}.bin");
+
+    let file_names = if http_client
+        .head(
+            release_downloads_url
+                .clone()
+                .join(&single_file_name)
+                .unwrap(),
+        )
+        .send()
+        .await
+        .and_then(Response::error_for_status)
+        .is_ok()
+    {
+        vec![single_file_name]
+    } else {
+        futures::stream::iter(ALPHABET.chars().flat_map(|first_char| {
+            ALPHABET.chars().map(move |second_char| {
+                format!("ppot_0080_{release_degree}_chunk_{first_char}{second_char}.bin")
+            })
+        }))
+        .take_while(|file_name| {
+            let file_name = file_name.clone();
+            let http_client = &http_client;
+            let release_downloads_url = release_downloads_url.clone();
+            async move {
+                http_client
+                    .head(release_downloads_url.join(&file_name).unwrap())
+                    .send()
+                    .await
+                    .and_then(Response::error_for_status)
+                    .is_ok()
+            }
+        })
+        .collect::<Vec<String>>()
+        .await
+    };
+
+    let file_paths = file_names
+        .iter()
+        .map(|file_name| directory.join(file_name))
+        .collect::<Vec<_>>();
+
+    let urls_and_paths = file_names
+        .iter()
+        .zip(file_paths.clone())
+        .filter_map(|(file_name, file_path)| {
+            file_path
+                .try_exists()
+                .map(|exists| {
+                    (!exists).then_some((
+                        release_downloads_url.clone().join(file_name).unwrap(),
+                        file_path,
+                    ))
+                })
+                .transpose()
+        })
+        .collect::<std::io::Result<Vec<_>>>()?;
+
+    try_join_all(urls_and_paths.into_iter().map(|(url, path)| {
+        let http_client = http_client.clone();
+        async move {
+            tokio::spawn(async move {
+                let file = Arc::new(Mutex::new(tokio::fs::File::create(path).await?));
+
+                log::info!("downloading hyperkzg setup chunk: {url}");
+                http_client
+                    .get(url.clone())
+                    .send()
+                    .await?
+                    .error_for_status()?
+                    .bytes_stream()
+                    .map_err(LoadPublicSetupError::from)
+                    .try_for_each(|bytes| {
+                        let file = file.clone();
+                        async move { Ok(file.lock().await.write_all(&bytes).await?) }
+                    })
+                    .await?;
+
+                log::info!("finished downloading hyperkzg setup chunk: {url}");
+
+                Result::<_, LoadPublicSetupError>::Ok(())
+            })
+            .await?
+        }
+    }))
+    .await?;
+
+    Ok(file_paths)
+}
+
 #[cfg(test)]
 pub mod tests {
     use ark_serialize::CanonicalSerialize;
     use clap::Parser;
 
     use super::*;
+    use crate::io::test_directory::TestDirectory;
 
     /// Test config that will load nu_1 setups from a file in this repository.
     pub fn sample_config_from_file() -> ProofOfSqlPublicSetupArgs {
@@ -126,6 +253,30 @@ pub mod tests {
     struct TestParser {
         #[command(flatten)]
         setup_args: ProofOfSqlPublicSetupArgs,
+    }
+
+    #[tokio::test]
+    async fn we_can_download_hyper_kzg_public_setup_files() {
+        let test_directory = TestDirectory::random(&mut rand::thread_rng());
+
+        let expected_file_path: PathBuf = test_directory.path.join("ppot_0080_03.bin");
+
+        // file does not exist yet
+        assert!(!tokio::fs::try_exists(&expected_file_path).await.unwrap());
+
+        // file gets downloaded
+        let file_names = download_hyperkzg_public_setup_files(&test_directory.path, "03")
+            .await
+            .unwrap();
+        assert_eq!(file_names, vec![expected_file_path.clone()]);
+        assert!(tokio::fs::try_exists(&expected_file_path).await.unwrap());
+
+        // file name still emitted and file still exists if file is already downloaded
+        let file_names = download_hyperkzg_public_setup_files(&test_directory.path, "03")
+            .await
+            .unwrap();
+        assert_eq!(file_names, vec![expected_file_path.clone()]);
+        assert!(tokio::fs::try_exists(&expected_file_path).await.unwrap());
     }
 
     #[tokio::test]
