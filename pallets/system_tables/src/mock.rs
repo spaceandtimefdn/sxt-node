1004ce7db8c8da28a7acc1dece50e0b2803c298a
Ada Anderson <ada.anderson@spaceandtime.io>
2025-03-27 10:16:39 -0400

    feat: Update mocks in system_tables pallet

--- pallets/system_tables/src/mock.rs
+++ pallets/system_tables/src/mock.rs
@@ -1,161 +1,215 @@
-// use frame_support::pallet_prelude::ConstU32;
-// use frame_support::traits::{ConstU128, VariantCountOf};
-// use frame_support::{derive_impl, parameter_types};
-// use sp_core::H256;
-// use sp_runtime::testing::UintAuthorityId;
-// use sp_runtime::traits::{IdentifyAccount, Verify};
-// use sp_runtime::{BuildStorage, FixedU128, MultiSignature};
-//
-// use crate as pallet_system_tables;
-//
-// type Block = frame_system::mocking::MockBlock<Test>;
-// type Balance = u128;
-// type BlockNumber = u64;
-// pub type Signature = sp_runtime::MultiSignature;
-// pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
-//
-// frame_support::construct_runtime!(
-//     pub enum Test
-//     {
-//         System: frame_system,
-//         SystemTables: pallet_system_tables, // Pallet under Test
-//         Balances: pallet_balances, // Required by Parsing
-//         Tables: pallet_tables, // Required by Parsing
-//         Permissions: pallet_permissions, // Required by Tables
-//         Staking: pallet_staking, // Required by Parsing
-//         Session: pallet_session, // Required by staking
-//     }
-// );
-//
-// #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
-// impl frame_system::Config for Test {
-//     type Block = Block;
-//     type Hash = H256;
-//     type AccountId = AccountId;
-//     type Lookup = IdentityLookup<AccountId>;
-// }
-//
-// impl pallet_system_tables::Config for Test {
-//     type RuntimeEvent = RuntimeEvent;
-// }
-//
-// parameter_types! {
-//     pub static SessionsPerEra: sp_staking::SessionIndex = 3;
-//     pub static ExistentialDeposit: Balance = 1;
-//     pub static SlashDeferDuration: sp_staking::EraIndex = 0;
-//     pub static Period: BlockNumber = 5;
-//     pub static Offset: BlockNumber = 0;
-//     pub static MaxControllersInDeprecationBatch: u32 = 5900;
-// }
-//
-// pub struct OtherSessionHandler;
-// impl frame_support::traits::OneSessionHandler<AccountId> for OtherSessionHandler {
-//     type Key = UintAuthorityId;
-//
-//     fn on_genesis_session<'a, I: 'a>(_: I)
-//     where
-//         I: Iterator<Item = (&'a AccountId, Self::Key)>,
-//         AccountId: 'a,
-//     {
-//     }
-//
-//     fn on_new_session<'a, I: 'a>(_: bool, _: I, _: I)
-//     where
-//         I: Iterator<Item = (&'a AccountId, Self::Key)>,
-//         AccountId: 'a,
-//     {
-//     }
-//
-//     fn on_disabled(_validator_index: u32) {}
-// }
-//
-// sp_runtime::impl_opaque_keys! {
-//     pub struct SessionKeys {
-//         pub other: AccountId,
-//     }
-// }
-//
-// impl pallet_session::Config for Test {
-//     type SessionManager = pallet_session::historical::NoteHistoricalRoot<Test, Staking>;
-//     type Keys = SessionKeys;
-//     type ShouldEndSession = pallet_session::PeriodicSessions<Period, Offset>;
-//     type SessionHandler = (OtherSessionHandler,);
-//     type RuntimeEvent = RuntimeEvent;
-//     type ValidatorId = AccountId;
-//     type ValidatorIdOf = pallet_staking::StashOf<Test>;
-//     type NextSessionRotation = pallet_session::PeriodicSessions<Period, Offset>;
-//     type WeightInfo = ();
-// }
-// impl pallet_permissions::Config for Test {
-//     type RuntimeEvent = RuntimeEvent;
-//     type WeightInfo = ();
-// }
-//
-// impl pallet_tables::Config for Test {
-//     type RuntimeEvent = RuntimeEvent;
-//     type WeightInfo = ();
-// }
-//
-// impl pallet_balances::Config for Test {
-//     /// The ubiquitous event type.
-//     type RuntimeEvent = RuntimeEvent;
-//     type RuntimeHoldReason = ();
-//     type RuntimeFreezeReason = ();
-//     type WeightInfo = pallet_balances::weights::SubstrateWeight<Test>;
-//     /// The type for recording an account's balance.
-//     type Balance = Balance;
-//     type DustRemoval = ();
-//     type ExistentialDeposit = ConstU128<100>;
-//     type AccountStore = System;
-//     type ReserveIdentifier = [u8; 8];
-//     type FreezeIdentifier = RuntimeFreezeReason;
-//     type MaxLocks = ConstU32<50>;
-//     type MaxReserves = ();
-//     type MaxFreezes = VariantCountOf<RuntimeFreezeReason>;
-// }
-//
-// /// Defines how much should the inflation be for an era given its duration.
-// pub struct EraPayout;
-// impl pallet_staking::EraPayout<Balance> for EraPayout {
-//     fn era_payout(
-//         _total_staked: Balance,
-//         _total_issuance: Balance,
-//         era_duration_millis: u64,
-//     ) -> (Balance, Balance) {
-//         // No payouts in testing right now
-//         (0, 0)
-//     }
-// }
-//
-// type ElectionProvider = frame_election_provider_support::NoElection<(
-//     AccountId,
-//     BlockNumber,
-//     pallet_staking::Pallet<Test>,
-//     frame_support::traits::ConstU32<1000>,
-// )>;
-//
-// impl pallet_staking::Config for Test {
-//     type Currency = Balances;
-//     type CurrencyBalance = <Self as pallet_balances::Config>::Balance;
-//     type UnixTime = ();
-//     type AdminOrigin = frame_system::EnsureRoot<Self::AccountId>;
-//     type SessionInterface = Self;
-//     type EraPayout = EraPayout;
-//     type NextNewSession = Session;
-//     type GenesisElectionProvider = Self::ElectionProvider;
-//     type VoterList = pallet_staking::UseNominatorsAndValidatorsMap<Self>;
-//     type TargetList = pallet_staking::UseValidatorsMap<Self>;
-// }
-// impl pallet_commitments::Config for Test {}
-//
-// pub fn new_test_ext() -> sp_io::TestExternalities {
-//     let mut storage = frame_system::GenesisConfig::<Test>::default()
-//         .build_storage()
-//         .unwrap();
-//
-//     pallet_commitments::GenesisConfig::<Test>::default()
-//         .assimilate_storage(&mut storage)
-//         .unwrap();
-//
-//     storage.into()
-// }
+use frame_election_provider_support::bounds::{ElectionBounds, ElectionBoundsBuilder};
+use frame_election_provider_support::{onchain, SequentialPhragmen};
+use frame_support::pallet_prelude::ConstU32;
+use frame_support::traits::{ConstU128, KeyOwnerProofSystem, VariantCountOf};
+use frame_support::{derive_impl, parameter_types};
+use pallet_grandpa::AuthorityId as GrandpaId;
+use proof_of_sql_static_setups::io::get_or_init_from_files_with_four_points_unchecked;
+use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
+use sp_consensus_babe::AuthorityId as BabeId;
+use sp_core::{ConstU64, H256};
+use sp_runtime::traits::{ConvertInto, IdentityLookup, MaybeConvert, OpaqueKeys, TryConvertInto};
+use sp_runtime::{generic, BuildStorage, KeyTypeId};
+
+use crate as pallet_system_tables;
+
+type Block = frame_system::mocking::MockBlock<Test>;
+
+frame_support::construct_runtime!(
+    pub enum Test
+    {
+        System: frame_system,
+        Permissions: pallet_permissions,
+        Commitments: pallet_commitments,
+        Tables: pallet_tables,
+        Session: pallet_session,
+        Historical: pallet_session::historical,
+        SystemTables: pallet_system_tables,
+        Balances: pallet_balances,
+        Staking: pallet_staking,
+        Babe: pallet_babe,
+        Grandpa: pallet_grandpa,
+        Authorship: pallet_authorship,
+        AuthorityDiscovery: pallet_authority_discovery,
+    }
+);
+
+type AccountId = u64;
+type Nonce = u32;
+type Balance = u64;
+
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
+    type Nonce = Nonce;
+    type AccountId = AccountId;
+    type AccountData = pallet_balances::AccountData<u64>;
+    type RuntimeCall = RuntimeCall;
+
+    type Block = Block;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Hash = H256;
+}
+
+pub const MAX_AUTHORITIES: u32 = 100_000u32;
+
+parameter_types! {
+    pub EpochDuration: u64 = 50 as u64;
+    pub const ExpectedBlockTime: u64 = 4000;
+    pub ReportLongevity: u64 = 100;
+}
+impl pallet_babe::Config for Test {
+    type EpochDuration = EpochDuration;
+    type ExpectedBlockTime = ExpectedBlockTime;
+    type EpochChangeTrigger = pallet_babe::ExternalTrigger;
+    type DisabledValidators = Session;
+    type WeightInfo = ();
+    type MaxAuthorities = ConstU32<MAX_AUTHORITIES>;
+    type MaxNominators = ConstU32<100_000>;
+    type KeyOwnerProof = <Historical as KeyOwnerProofSystem<(KeyTypeId, BabeId)>>::Proof;
+    type EquivocationReportSystem = ();
+}
+
+impl pallet_authorship::Config for Test {
+    type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Babe>;
+    type EventHandler = (Staking);
+}
+
+impl pallet_grandpa::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
+    type MaxAuthorities = ConstU32<MAX_AUTHORITIES>;
+    type MaxNominators = ConstU32<0>;
+    type MaxSetIdSessionEntries = ConstU64<0>;
+    type KeyOwnerProof = <Historical as KeyOwnerProofSystem<(KeyTypeId, BabeId)>>::Proof;
+    type EquivocationReportSystem = ();
+}
+impl pallet_authority_discovery::Config for Test {
+    type MaxAuthorities = ConstU32<MAX_AUTHORITIES>;
+}
+
+#[derive_impl(pallet_balances::config_preludes::TestDefaultConfig)]
+impl pallet_balances::Config for Test {
+    type AccountStore = System;
+}
+
+impl pallet_timestamp::Config for Test {
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type MinimumPeriod = ConstU64<5>;
+    type WeightInfo = ();
+}
+
+pallet_staking_reward_curve::build! {
+    const I_NPOS: sp_runtime::curve::PiecewiseLinear<'static> = curve!(
+        min_inflation: 0_025_000,
+        max_inflation: 0_100_000,
+        ideal_stake: 0_500_000,
+        falloff: 0_050_000,
+        max_piece_count: 40,
+        test_precision: 0_005_000,
+    );
+}
+parameter_types! {
+    pub const RewardCurve: &'static sp_runtime::curve::PiecewiseLinear<'static> = &I_NPOS;
+    pub static ElectionsBounds: ElectionBounds = ElectionBoundsBuilder::default().build();
+}
+
+pub struct OnChainSeqPhragmen;
+impl onchain::Config for OnChainSeqPhragmen {
+    type System = Test;
+    type Solver = SequentialPhragmen<AccountId, sp_runtime::Perbill>;
+    type DataProvider = Staking;
+    type WeightInfo = ();
+    type MaxWinners = ConstU32<100>;
+    type Bounds = ElectionsBounds;
+}
+
+#[derive_impl(pallet_staking::config_preludes::TestDefaultConfig)]
+impl pallet_staking::Config for Test {
+    type Currency = Balances;
+    type CurrencyBalance = <Self as pallet_balances::Config>::Balance;
+    type UnixTime = pallet_timestamp::Pallet<Self>;
+    type AdminOrigin = frame_system::EnsureRoot<Self::AccountId>;
+    type SessionInterface = Self;
+    type EraPayout = pallet_staking::ConvertCurve<RewardCurve>;
+    type NextNewSession = Session;
+    type ElectionProvider = onchain::OnChainExecution<OnChainSeqPhragmen>;
+    type GenesisElectionProvider = Self::ElectionProvider;
+    type VoterList = pallet_staking::UseNominatorsAndValidatorsMap<Self>;
+    type TargetList = pallet_staking::UseValidatorsMap<Self>;
+}
+
+pub type BlockNumber = u64;
+
+parameter_types! {
+        pub const Period: BlockNumber = 1;
+        pub const Offset: BlockNumber = 0;
+}
+
+pub struct TestSessionHandler;
+impl pallet_session::SessionHandler<AccountId> for TestSessionHandler {
+    const KEY_TYPE_IDS: &'static [KeyTypeId] = &[sp_core::crypto::key_types::DUMMY];
+
+    fn on_new_session<Ks: OpaqueKeys>(
+        _changed: bool,
+        _validators: &[(AccountId, Ks)],
+        _queued_validators: &[(AccountId, Ks)],
+    ) {
+    }
+
+    fn on_disabled(_validator_index: u32) {}
+
+    fn on_genesis_session<Ks: OpaqueKeys>(_validators: &[(AccountId, Ks)]) {}
+}
+
+impl pallet_session::Config for Test {
+    type SessionManager = ();
+    type Keys = sp_runtime::testing::UintAuthorityId;
+    type ShouldEndSession = pallet_session::PeriodicSessions<Period, Offset>;
+    type SessionHandler = TestSessionHandler;
+    type RuntimeEvent = RuntimeEvent;
+    type ValidatorId = AccountId;
+    type ValidatorIdOf = pallet_staking::StashOf<Test>;
+    type NextSessionRotation = pallet_session::PeriodicSessions<Period, Offset>;
+    type WeightInfo = ();
+}
+impl pallet_session::historical::Config for Test {
+    type FullIdentification = pallet_staking::Exposure<AccountId, Balance>;
+    type FullIdentificationOf = pallet_staking::ExposureOf<Test>;
+}
+
+sp_runtime::impl_opaque_keys! {
+    pub struct SessionKeys {
+            pub babe: Babe,
+            pub grandpa: Grandpa,
+            pub authority_discovery: AuthorityDiscovery,
+    }
+}
+
+impl pallet_system_tables::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+}
+
+impl pallet_tables::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
+}
+
+impl pallet_permissions::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
+}
+impl pallet_commitments::Config for Test {}
+
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    let _ = get_or_init_from_files_with_four_points_unchecked();
+    let mut storage = frame_system::GenesisConfig::<Test>::default()
+        .build_storage()
+        .unwrap();
+
+    pallet_commitments::GenesisConfig::<Test>::default()
+        .assimilate_storage(&mut storage)
+        .unwrap();
+
+    storage.into()
+}
