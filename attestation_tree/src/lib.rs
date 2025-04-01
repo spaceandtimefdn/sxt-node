642e555c17414abec3b1a68128ad0920247afd13
Trevor Lovell <trevor.lovell@spaceandtime.io>
2025-04-01 01:42:20 -0600

    feat: add PrefixFoliate trait to attestation_tree
    PrefixFoliate is the main abstraction in the attestation_tree crate. It
    allows users to define a prefix to be encoded in the attestation tree,
    and how to encode its leaves given strong types.

--- attestation_tree/src/lib.rs
+++ attestation_tree/src/lib.rs
@@ -2,3 +2,11 @@
 
 mod hash_and_key;
 pub use hash_and_key::{HashAndKey, HashAndKeyTuple};
+
+mod prefix_foliate;
+pub use prefix_foliate::{
+    decode_storage_key_and_value,
+    storage_key_for_prefix_key_tuple,
+    DecodeStorageError,
+    PrefixFoliate,
+};
