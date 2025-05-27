4a07a0ad40c9ec7e42cc007daa5a5a6e0520fad7
Zach Frederick <zach.frederick@spaceandtime.io>
2025-05-27 14:24:33 -0400

    feat: add zkpay to system staking contracts

--- snapshots/v4/sxt_system_staking/ddl_sxt_system_staking.sql
+++ snapshots/v4/sxt_system_staking/ddl_sxt_system_staking.sql
@@ -144,3 +144,188 @@ CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.STAKINGTOKENSET(
     token binary not null,
     primary key (block_number, transaction_hash, event_index)
 );
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.ASSETADDED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    ASSET BINARY NOT NULL,
+    ALLOWEDPAYMENTTYPES BINARY NOT NULL,
+    PRICEFEED BINARY NOT NULL,
+    TOKENDECIMALS SMALLINT NOT NULL,
+    STALEPRICETHRESHOLDINSECONDS DECIMAL(20,0) NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.ASSETREMOVED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    ASSET BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.CALLBACKFAILED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    CALLBACKCLIENTCONTRACTADDRESS BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.CALLBACKSUCCEEDED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    CALLBACKCLIENTCONTRACTADDRESS BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.INITIALIZED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    VERSION DECIMAL(20,0) NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.NEWQUERYPAYMENT (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    ASSET BINARY NOT NULL,
+    AMOUNT DECIMAL(75,0) NOT NULL,
+    SOURCE_ BINARY NOT NULL,
+    AMOUNTINUSD DECIMAL(75,0) NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.OWNERSHIPTRANSFERRED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    PREVIOUSOWNER BINARY NOT NULL,
+    NEWOWNER BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.PAYMENTREFUNDED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    ASSET BINARY NOT NULL,
+    SOURCE_ BINARY NOT NULL,
+    AMOUNT DECIMAL(75,0) NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.PAYMENTSETTLED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    USEDAMOUNT DECIMAL(75,0) NOT NULL,
+    REMAININGAMOUNT DECIMAL(75,0) NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.QUERYCANCELED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    CALLER BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.QUERYFULFILLED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.QUERYRECEIVED (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    QUERYNONCE DECIMAL(75,0) NOT NULL,
+    SENDER BINARY NOT NULL,
+    QUERY BINARY NOT NULL,
+    QUERYPARAMETERS BINARY NOT NULL,
+    TIMEOUT DECIMAL(20,0) NOT NULL,
+    CALLBACKCLIENTCONTRACTADDRESS BINARY NOT NULL,
+    CALLBACKGASLIMIT DECIMAL(20,0) NOT NULL,
+    CALLBACKDATA BINARY NOT NULL,
+    CUSTOMLOGICCONTRACTADDRESS BINARY NOT NULL,
+    QUERYHASH BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.SENDPAYMENT (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    ASSET BINARY NOT NULL,
+    AMOUNT DECIMAL(75,0) NOT NULL,
+    ONBEHALFOF BINARY NOT NULL,
+    TARGET BINARY NOT NULL,
+    MEMO BINARY NOT NULL,
+    AMOUNTINUSD DECIMAL(75,0) NOT NULL,
+    SENDER BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
+
+
+CREATE TABLE IF NOT EXISTS SXT_SYSTEM_STAKING.TREASURYSET (
+    block_number BIGINT NOT NULL,
+    transaction_hash BINARY NOT NULL,
+    event_index INTEGER NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    contract_address BINARY NOT NULL,
+    TREASURY BINARY NOT NULL,
+    PRIMARY KEY (block_number, transaction_hash, event_index)
+);
