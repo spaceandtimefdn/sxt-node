5b1cc0d4630e5177d67d4e2e3d8b83b172e6557f
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-07-08 19:24:07 -0600

    refactor: move tests to bottom of file in sxt-core tables module
    Currently, the tests in the tables module are in sort of an unusual
    place, in the middle of the file. It'll be less of a burden to add more
    code to this file, and more tests of said code, if the tests are at the
    standard location - the bottom.

--- sxt-core/src/tables.rs
+++ sxt-core/src/tables.rs
@@ -739,6 +739,175 @@ pub fn generate_uuid(source: String) -> Option<TableUuid> {
     }
 }
 
+/// The type of table that we are indexing
+#[derive(
+    Clone,
+    Encode,
+    Decode,
+    Eq,
+    PartialEq,
+    RuntimeDebug,
+    TypeInfo,
+    MaxEncodedLen,
+    Default,
+    Serialize,
+    Deserialize,
+)]
+pub enum TableType {
+    /// Core Blockchain table
+    #[default]
+    CoreBlockchain,
+
+    /// Smart Contract Indexing
+    SCI,
+
+    /// Community Owned Table
+    Community,
+
+    /// Testing type
+    Testing(InsertQuorumSize),
+}
+
+impl From<TableType> for InsertQuorumSize {
+    fn from(table_type: TableType) -> Self {
+        match table_type {
+            TableType::CoreBlockchain => InsertQuorumSize {
+                public: Some(3),
+                privileged: None,
+            },
+            TableType::SCI => InsertQuorumSize {
+                public: Some(1),
+                privileged: None,
+            },
+            TableType::Community => InsertQuorumSize {
+                public: None,
+                privileged: Some(0),
+            },
+            TableType::Testing(quorum) => quorum,
+        }
+    }
+}
+
+/// Commitment schemes
+#[derive(
+    Copy,
+    Clone,
+    Encode,
+    Decode,
+    Eq,
+    PartialEq,
+    RuntimeDebug,
+    TypeInfo,
+    MaxEncodedLen,
+    Serialize,
+    Deserialize,
+)]
+pub enum CommitmentScheme {
+    /// HyperKzg
+    HyperKzg,
+    /// dynamic dory
+    DynamicDory,
+}
+
+/// A list of column names to column types.
+pub type TableSchema = Vec<ScaleColumnSchema>;
+
+/// Simple column schema in a form that can be communicated in chain APIs.
+#[derive(Clone, PartialEq, Eq, Debug, Encode, Decode, TypeInfo)]
+pub struct ScaleColumnSchema {
+    /// The name of the column.
+    name: String,
+    /// The data type of the column, as plain SQL.
+    data_type: String,
+}
+
+impl From<ColumnDef> for ScaleColumnSchema {
+    fn from(
+        ColumnDef {
+            name, data_type, ..
+        }: ColumnDef,
+    ) -> Self {
+        ScaleColumnSchema {
+            name: name.value,
+            data_type: data_type.to_string(),
+        }
+    }
+}
+
+impl TryFrom<ScaleColumnSchema> for ColumnDef {
+    type Error = sqlparser::parser::ParserError;
+
+    fn try_from(
+        ScaleColumnSchema { name, data_type }: ScaleColumnSchema,
+    ) -> Result<Self, Self::Error> {
+        let name = Ident::new(name);
+
+        let parser = Parser::new(&PostgreSqlDialect {});
+
+        let data_type = parser.try_with_sql(&data_type).and_then(|mut parser| {
+            let data_type = parser.parse_data_type()?;
+
+            if parser.peek_token() == Token::EOF {
+                Ok(data_type)
+            } else {
+                Err(sqlparser::parser::ParserError::ParserError(
+                    "expected data type string to be a single data type".to_string(),
+                ))
+            }
+        })?;
+
+        Ok(ColumnDef {
+            name,
+            data_type,
+            collation: None,
+            options: Vec::new(),
+        })
+    }
+}
+
+fn table_schema_from_create_table(create_table: CreateTableBuilder) -> TableSchema {
+    create_table
+        .columns
+        .into_iter()
+        .map(ScaleColumnSchema::from)
+        .collect()
+}
+
+/// Returns a [`TableSchema`] inferred from the given [`CreateStatement`].
+pub fn table_schema_from_create_statement(
+    create_statement: CreateStatement,
+) -> Result<TableSchema, CreateStatementParseError> {
+    create_statement_to_sqlparser(create_statement).map(table_schema_from_create_table)
+}
+
+/// Errors that can occur when getting a table schema from chain state.
+#[derive(Snafu, Debug, Decode, Encode, TypeInfo)]
+#[snafu(module)]
+pub enum GetTableSchemaError {
+    #[snafu(display("table does not exist"))]
+    /// Table does not exist.
+    NoSuchTable,
+    #[snafu(display("stored table definition exceeds maximum size"))]
+    /// Stored table definition exceeds maximum size.
+    StatementTooLarge,
+    #[snafu(display("create statement does not store valid utf8"))]
+    /// Create statement does not store valid utf8.
+    NotUtf8,
+    #[snafu(display("failed to parse create statement"))]
+    /// Failed to parse create statement.
+    Parse,
+}
+
+impl From<CreateStatementParseError> for GetTableSchemaError {
+    fn from(error: CreateStatementParseError) -> Self {
+        match error {
+            CreateStatementParseError::StatementTooLarge => GetTableSchemaError::StatementTooLarge,
+            CreateStatementParseError::Utf8 { .. } => GetTableSchemaError::NotUtf8,
+            CreateStatementParseError::Sqlparser { .. } => GetTableSchemaError::Parse,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use alloc::string::String;
@@ -1155,174 +1324,5 @@ mod tests {
         let create_statement = create_statement("SELECT * FROM NOT.CREATE");
 
         assert!(table_schema_from_create_statement(create_statement).is_err());
-    }
-}
-
-/// The type of table that we are indexing
-#[derive(
-    Clone,
-    Encode,
-    Decode,
-    Eq,
-    PartialEq,
-    RuntimeDebug,
-    TypeInfo,
-    MaxEncodedLen,
-    Default,
-    Serialize,
-    Deserialize,
-)]
-pub enum TableType {
-    /// Core Blockchain table
-    #[default]
-    CoreBlockchain,
-
-    /// Smart Contract Indexing
-    SCI,
-
-    /// Community Owned Table
-    Community,
-
-    /// Testing type
-    Testing(InsertQuorumSize),
-}
-
-impl From<TableType> for InsertQuorumSize {
-    fn from(table_type: TableType) -> Self {
-        match table_type {
-            TableType::CoreBlockchain => InsertQuorumSize {
-                public: Some(3),
-                privileged: None,
-            },
-            TableType::SCI => InsertQuorumSize {
-                public: Some(1),
-                privileged: None,
-            },
-            TableType::Community => InsertQuorumSize {
-                public: None,
-                privileged: Some(0),
-            },
-            TableType::Testing(quorum) => quorum,
-        }
-    }
-}
-
-/// Commitment schemes
-#[derive(
-    Copy,
-    Clone,
-    Encode,
-    Decode,
-    Eq,
-    PartialEq,
-    RuntimeDebug,
-    TypeInfo,
-    MaxEncodedLen,
-    Serialize,
-    Deserialize,
-)]
-pub enum CommitmentScheme {
-    /// HyperKzg
-    HyperKzg,
-    /// dynamic dory
-    DynamicDory,
-}
-
-/// A list of column names to column types.
-pub type TableSchema = Vec<ScaleColumnSchema>;
-
-/// Simple column schema in a form that can be communicated in chain APIs.
-#[derive(Clone, PartialEq, Eq, Debug, Encode, Decode, TypeInfo)]
-pub struct ScaleColumnSchema {
-    /// The name of the column.
-    name: String,
-    /// The data type of the column, as plain SQL.
-    data_type: String,
-}
-
-impl From<ColumnDef> for ScaleColumnSchema {
-    fn from(
-        ColumnDef {
-            name, data_type, ..
-        }: ColumnDef,
-    ) -> Self {
-        ScaleColumnSchema {
-            name: name.value,
-            data_type: data_type.to_string(),
-        }
-    }
-}
-
-impl TryFrom<ScaleColumnSchema> for ColumnDef {
-    type Error = sqlparser::parser::ParserError;
-
-    fn try_from(
-        ScaleColumnSchema { name, data_type }: ScaleColumnSchema,
-    ) -> Result<Self, Self::Error> {
-        let name = Ident::new(name);
-
-        let parser = Parser::new(&PostgreSqlDialect {});
-
-        let data_type = parser.try_with_sql(&data_type).and_then(|mut parser| {
-            let data_type = parser.parse_data_type()?;
-
-            if parser.peek_token() == Token::EOF {
-                Ok(data_type)
-            } else {
-                Err(sqlparser::parser::ParserError::ParserError(
-                    "expected data type string to be a single data type".to_string(),
-                ))
-            }
-        })?;
-
-        Ok(ColumnDef {
-            name,
-            data_type,
-            collation: None,
-            options: Vec::new(),
-        })
-    }
-}
-
-fn table_schema_from_create_table(create_table: CreateTableBuilder) -> TableSchema {
-    create_table
-        .columns
-        .into_iter()
-        .map(ScaleColumnSchema::from)
-        .collect()
-}
-
-/// Returns a [`TableSchema`] inferred from the given [`CreateStatement`].
-pub fn table_schema_from_create_statement(
-    create_statement: CreateStatement,
-) -> Result<TableSchema, CreateStatementParseError> {
-    create_statement_to_sqlparser(create_statement).map(table_schema_from_create_table)
-}
-
-/// Errors that can occur when getting a table schema from chain state.
-#[derive(Snafu, Debug, Decode, Encode, TypeInfo)]
-#[snafu(module)]
-pub enum GetTableSchemaError {
-    #[snafu(display("table does not exist"))]
-    /// Table does not exist.
-    NoSuchTable,
-    #[snafu(display("stored table definition exceeds maximum size"))]
-    /// Stored table definition exceeds maximum size.
-    StatementTooLarge,
-    #[snafu(display("create statement does not store valid utf8"))]
-    /// Create statement does not store valid utf8.
-    NotUtf8,
-    #[snafu(display("failed to parse create statement"))]
-    /// Failed to parse create statement.
-    Parse,
-}
-
-impl From<CreateStatementParseError> for GetTableSchemaError {
-    fn from(error: CreateStatementParseError) -> Self {
-        match error {
-            CreateStatementParseError::StatementTooLarge => GetTableSchemaError::StatementTooLarge,
-            CreateStatementParseError::Utf8 { .. } => GetTableSchemaError::NotUtf8,
-            CreateStatementParseError::Sqlparser { .. } => GetTableSchemaError::Parse,
-        }
     }
 }
