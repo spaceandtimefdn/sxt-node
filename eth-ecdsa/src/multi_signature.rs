7e32f0b8e14b4bb8d8cdf291dd60bf1f361a3039
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-08-08 02:22:53 -0600

    feat: add eth-ecdsa as a 4th runtime signature type
    While we can already sign transactions with ECDSA, the semantics of
    ECDSA signing are a bit different from ethereum. This change adds a
    crate with a new signature type for ECDSA signatures that are a bit more
    "ethereum compatible", as they expect payloads to be signed following
    EIP-191. It also adds a new MultiSignature type whose encoding is
    backwards compatible with sp_runtime's MultiSignature, just with an
    extra EthEcdsa variant.

---
+++ eth-ecdsa/src/multi_signature.rs
@@ -0,0 +1,169 @@
+use codec::{Decode, Encode, MaxEncodedLen};
+use scale_info::TypeInfo;
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+use snafu::Snafu;
+use sp_core::{ecdsa, ed25519, sr25519, RuntimeDebug};
+use sp_runtime::traits::{IdentifyAccount, Lazy, Verify};
+use sp_runtime::AccountId32;
+
+use crate::{EthEcdsaSignature, EthEcdsaSigner};
+
+/// Signature verify that can work with any known signature types.
+///
+/// Implemented similarly to `sp_runtime::MultiSignature` but with an extra variant `EthEcdsa`.
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Eq, PartialEq, Clone, Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
+pub enum MultiSignature {
+    /// An Ed25519 signature.
+    Ed25519(ed25519::Signature),
+    /// An Sr25519 signature.
+    Sr25519(sr25519::Signature),
+    /// An ECDSA/SECP256k1 signature.
+    Ecdsa(ecdsa::Signature),
+    /// An Eth ecdsa signature of EIP-191 data.
+    EthEcdsa(EthEcdsaSignature),
+}
+
+/// Public key for any known crypto algorithm.
+///
+/// Implemented similarly to `sp_runtime::MultiSignature` but with an extra variant `EthEcdsa`.
+#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum MultiSigner {
+    /// An Ed25519 identity.
+    Ed25519(ed25519::Public),
+    /// An Sr25519 identity.
+    Sr25519(sr25519::Public),
+    /// An SECP256k1/ECDSA identity (actually, the Blake2 hash of the compressed pub key).
+    Ecdsa(ecdsa::Public),
+    /// An Ethereum SECP256k1/ECDSA identity, with the ethereum account id prepended by 12 0-bytes.
+    EthEcdsa(EthEcdsaSigner),
+}
+
+/// Encountered EthEcdsa variant unexpectedly.
+#[derive(Debug, Snafu)]
+#[snafu(display("encountered EthEcdsa variant unexpectedly"))]
+pub struct EncounteredEthEcdsa;
+
+impl From<sp_runtime::MultiSignature> for MultiSignature {
+    fn from(value: sp_runtime::MultiSignature) -> Self {
+        match value {
+            sp_runtime::MultiSignature::Ed25519(signature) => MultiSignature::Ed25519(signature),
+            sp_runtime::MultiSignature::Sr25519(signature) => MultiSignature::Sr25519(signature),
+            sp_runtime::MultiSignature::Ecdsa(signature) => MultiSignature::Ecdsa(signature),
+        }
+    }
+}
+
+impl TryFrom<MultiSignature> for sp_runtime::MultiSignature {
+    type Error = EncounteredEthEcdsa;
+
+    fn try_from(value: MultiSignature) -> Result<Self, Self::Error> {
+        match value {
+            MultiSignature::Ed25519(signature) => {
+                Ok(sp_runtime::MultiSignature::Ed25519(signature))
+            }
+            MultiSignature::Sr25519(signature) => {
+                Ok(sp_runtime::MultiSignature::Sr25519(signature))
+            }
+            MultiSignature::Ecdsa(signature) => Ok(sp_runtime::MultiSignature::Ecdsa(signature)),
+            MultiSignature::EthEcdsa(_) => Err(EncounteredEthEcdsa),
+        }
+    }
+}
+
+impl From<sp_runtime::MultiSigner> for MultiSigner {
+    fn from(value: sp_runtime::MultiSigner) -> Self {
+        match value {
+            sp_runtime::MultiSigner::Ed25519(signature) => MultiSigner::Ed25519(signature),
+            sp_runtime::MultiSigner::Sr25519(signature) => MultiSigner::Sr25519(signature),
+            sp_runtime::MultiSigner::Ecdsa(signature) => MultiSigner::Ecdsa(signature),
+        }
+    }
+}
+
+impl TryFrom<MultiSigner> for sp_runtime::MultiSigner {
+    type Error = EncounteredEthEcdsa;
+
+    fn try_from(value: MultiSigner) -> Result<Self, Self::Error> {
+        match value {
+            MultiSigner::Ed25519(signature) => Ok(sp_runtime::MultiSigner::Ed25519(signature)),
+            MultiSigner::Sr25519(signature) => Ok(sp_runtime::MultiSigner::Sr25519(signature)),
+            MultiSigner::Ecdsa(signature) => Ok(sp_runtime::MultiSigner::Ecdsa(signature)),
+            MultiSigner::EthEcdsa(_) => Err(EncounteredEthEcdsa),
+        }
+    }
+}
+
+impl IdentifyAccount for MultiSigner {
+    type AccountId = AccountId32;
+    fn into_account(self) -> AccountId32 {
+        match self {
+            MultiSigner::EthEcdsa(eth_ecdsa_signer) => eth_ecdsa_signer.into_account(),
+            sp_signer => sp_runtime::MultiSigner::try_from(sp_signer)
+                .expect("this is not eth-ecdsa")
+                .into_account(),
+        }
+    }
+}
+
+impl Verify for MultiSignature {
+    type Signer = MultiSigner;
+    fn verify<L: Lazy<[u8]>>(&self, msg: L, signer: &AccountId32) -> bool {
+        match self {
+            Self::EthEcdsa(eth_ecdsa_sig) => eth_ecdsa_sig.verify(msg, signer),
+            sp_signature => sp_runtime::MultiSignature::try_from(sp_signature.clone())
+                .expect("this is not eth-ecdsa")
+                .verify(msg, signer),
+        }
+    }
+}
+
+impl From<ed25519::Signature> for MultiSignature {
+    fn from(x: ed25519::Signature) -> Self {
+        sp_runtime::MultiSignature::from(x).into()
+    }
+}
+
+impl From<ed25519::Public> for MultiSigner {
+    fn from(x: ed25519::Public) -> Self {
+        sp_runtime::MultiSigner::from(x).into()
+    }
+}
+
+impl From<sr25519::Signature> for MultiSignature {
+    fn from(x: sr25519::Signature) -> Self {
+        sp_runtime::MultiSignature::from(x).into()
+    }
+}
+
+impl From<sr25519::Public> for MultiSigner {
+    fn from(x: sr25519::Public) -> Self {
+        sp_runtime::MultiSigner::from(x).into()
+    }
+}
+
+impl From<ecdsa::Signature> for MultiSignature {
+    fn from(x: ecdsa::Signature) -> Self {
+        sp_runtime::MultiSignature::from(x).into()
+    }
+}
+
+impl From<ecdsa::Public> for MultiSigner {
+    fn from(x: ecdsa::Public) -> Self {
+        sp_runtime::MultiSigner::from(x).into()
+    }
+}
+
+impl From<EthEcdsaSignature> for MultiSignature {
+    fn from(x: EthEcdsaSignature) -> Self {
+        MultiSignature::EthEcdsa(x)
+    }
+}
+
+impl From<EthEcdsaSigner> for MultiSigner {
+    fn from(x: EthEcdsaSigner) -> Self {
+        MultiSigner::EthEcdsa(x)
+    }
+}
---
+++ eth-ecdsa/src/signature.rs
@@ -0,0 +1,122 @@
+use alloy_primitives::Address;
+use codec::{Decode, Encode, MaxEncodedLen};
+use k256::ecdsa::VerifyingKey;
+use scale_info::TypeInfo;
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+use sp_core::{ecdsa, RuntimeDebug};
+use sp_runtime::traits::{IdentifyAccount, Lazy, Verify};
+use sp_runtime::AccountId32;
+
+/// Wrapper type over an ECDSA signature (a 512-bit value, plus 8 bits for recovery ID).
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Eq, PartialEq, Clone, Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
+pub struct EthEcdsaSignature(pub ecdsa::Signature);
+
+/// Wrapper type over an ECDSA compressed public key.
+#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct EthEcdsaSigner(pub ecdsa::Public);
+
+impl IdentifyAccount for EthEcdsaSigner {
+    type AccountId = AccountId32;
+    fn into_account(self) -> AccountId32 {
+        (*Address::from_public_key(
+            &VerifyingKey::from_sec1_bytes(&self.0)
+                .expect("ecdsa::Public key should be in compressed form"),
+        )
+        .into_word())
+        .into()
+    }
+}
+
+impl Verify for EthEcdsaSignature {
+    type Signer = EthEcdsaSigner;
+    fn verify<L: Lazy<[u8]>>(&self, mut msg: L, signer: &AccountId32) -> bool {
+        let Ok(alloy_sig) = alloy_primitives::PrimitiveSignature::from_raw_array(&self.0 .0) else {
+            return false;
+        };
+
+        let Ok(address) = alloy_sig.recover_address_from_msg(msg.get()) else {
+            return false;
+        };
+
+        &AccountId32::from(*address.into_word()) == signer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloy_signer::SignerSync;
+    use alloy_signer_local::PrivateKeySigner;
+
+    use super::*;
+
+    #[test]
+    fn we_can_identify_account_id() {
+        let signer = PrivateKeySigner::random();
+
+        let verifying_key = signer.credential().verifying_key();
+
+        let eth_ecdsa_signer =
+            EthEcdsaSigner(ecdsa::Public::try_from(&verifying_key.to_sec1_bytes()[..]).unwrap());
+
+        let address = signer.address();
+
+        let expected_account_id = AccountId32::new(
+            core::iter::repeat(0)
+                .take(12)
+                .chain(address.into_array())
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(),
+        );
+
+        assert_eq!(eth_ecdsa_signer.into_account(), expected_account_id)
+    }
+
+    fn valid_verification_input() -> (EthEcdsaSignature, Vec<u8>, AccountId32) {
+        let signer = PrivateKeySigner::random();
+        let message = b"Hello World!";
+
+        let address = signer.address();
+
+        let account_id = AccountId32::new(
+            core::iter::repeat(0)
+                .take(12)
+                .chain(address.into_array())
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(),
+        );
+
+        let signature = signer.sign_message_sync(message).unwrap();
+
+        let eth_ecdsa_signature = EthEcdsaSignature(signature.as_bytes().into());
+
+        (eth_ecdsa_signature, message.to_vec(), account_id)
+    }
+
+    #[test]
+    fn we_can_verify_signature() {
+        let (eth_ecdsa_signature, message, account_id) = valid_verification_input();
+
+        assert!(eth_ecdsa_signature.verify(&message[..], &account_id));
+    }
+
+    #[test]
+    fn we_cannot_verify_signature_with_wrong_message() {
+        let (eth_ecdsa_signature, _, account_id) = valid_verification_input();
+
+        assert!(!eth_ecdsa_signature.verify(&b"Goodbye."[..], &account_id));
+    }
+
+    #[test]
+    fn we_cannot_verify_signature_with_wrong_account() {
+        let (eth_ecdsa_signature, message, mut account_id) = valid_verification_input();
+
+        let account_id_array = <sp_runtime::AccountId32 as AsMut<[u8]>>::as_mut(&mut account_id);
+        account_id_array[0] = account_id_array[0].wrapping_add(1);
+        assert!(!eth_ecdsa_signature.verify(&message[..], &account_id));
+    }
+}
