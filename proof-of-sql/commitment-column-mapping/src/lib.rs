6b908670ec3d9326e1c7170ad6ce2ab5fc0ab8c8
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-08-10 00:06:49 -0600

    feat: add commitment-column-mapping crate
    There aren't many utilities for editing commitments currently. We
    haven't had to yet. This adds a crate for editing commitments following
    some of rust's functional paradigms on a columnar basis, along with
    OnChainTables.
    
    This will soon be used to alter the way VarChar columns are committed to
    as a pure runtime upgrade to be more EVM-compatible, despite the fact
    that commitments are computed in the native interfaces.

---
+++ proof-of-sql/commitment-column-mapping/src/lib.rs
@@ -0,0 +1,14 @@
+#![doc = include_str!("../README.md")]
+#![no_std]
+
+extern crate alloc;
+
+mod map_column_commitment_metadata;
+
+mod try_map_on_chain_column;
+
+mod combinator;
+
+mod map_table_commitment;
+
+mod map_on_chain_table;
