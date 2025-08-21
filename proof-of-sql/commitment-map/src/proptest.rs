e1ef96025fe80bcddf83dcf63f60e1d38bdb3fad
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-08-20 21:45:36 -0600

    feat: add proptest strategies for basic commitment scheme types

---
+++ proof-of-sql/commitment-map/src/proptest.rs
@@ -0,0 +1,14 @@
+//! Strategies for generating commitment scheme types for use in tests.
+
+use proptest::prelude::*;
+
+use crate::CommitmentSchemeFlags;
+
+prop_compose! {
+    pub fn commitment_scheme_flags()(weight in 1u8..4) -> CommitmentSchemeFlags {
+        CommitmentSchemeFlags {
+            hyper_kzg: weight % 2 == 1,
+            dynamic_dory: weight >> 1 % 2 == 1,
+        }
+    }
+}
