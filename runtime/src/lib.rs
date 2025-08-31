9476503fa6f557c562ec182e3601ac59d69f6183
Jay White <JayWhite2357@users.noreply.github.com>
2025-08-30 23:04:20 -0400

    feat: adjust gas pricing based on testnet metrics

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -165,7 +165,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     //   `spec_version`, and `authoring_version` are the same between Wasm and native.
     // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
     //   the compatible custom types.
-    spec_version: 233,
+    spec_version: 234,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
@@ -357,10 +357,10 @@ impl pallet_utility::Config for Runtime {
 /// We want to base our pricing on the cost of the data insertion transaction since this is the
 /// most common action on the network. The values below are intended to represent an 'Average'
 /// Insert of 5000 bytes of data.
-pub const CALIBRATION_MULTIPLIER: u128 = 10; // A Calibration multiplier to reach the desired target pricing
+pub const CALIBRATION_MULTIPLIER: u128 = 43; // A Calibration multiplier to reach the desired target pricing
 pub const AVERAGE_INSERT_SIZE_BYTES: u128 = 8192;
-pub const AVERAGE_INSERT_TARGET_COST: u128 = CENTS
-    .saturating_mul(5)
+pub const AVERAGE_INSERT_TARGET_COST: u128 = MILLICENTS
+    .saturating_mul(20)
     .saturating_mul(CALIBRATION_MULTIPLIER);
 pub const TARGET_BYTE_FEE: u128 =
     AVERAGE_INSERT_TARGET_COST.saturating_div(AVERAGE_INSERT_SIZE_BYTES);
