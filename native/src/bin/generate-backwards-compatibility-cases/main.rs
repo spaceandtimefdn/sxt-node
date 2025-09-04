703883fae508d71feabfad3f718a4eda4996c967
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-09-04 09:14:46 -0600

    test: add binary for generating native backwards compatibility cases
    We must never break the functionality of native interfaces, as it will
    break any new node's ability to sync up from genesis. We currently don't
    have any CI that verifies this backwards compatibility requirement. We
    want some test cases that hard-code the native pass-by serialization of
    their input/output. That way, if the function breaks, or if even the
    serialization breaks, it will be caught in tests.
    
    This adds a binary to the native crate -
    generate-backwards-compatibility-cases. This generates hundreds of
    hard-coded cases for the process_insert native interface.

---
+++ native/src/bin/generate-backwards-compatibility-cases/main.rs
@@ -0,0 +1,40 @@
+#![doc = include_str!("README.md")]
+use clap::{Parser, ValueEnum};
+
+mod write_cases;
+pub use write_cases::write_cases;
+
+mod process_insert;
+
+/// The function to generate cases for.
+#[derive(Debug, Clone, ValueEnum)]
+enum Function {
+    /// native::interface::process_insert
+    ProcessInsert,
+}
+
+/// Generate "cases" for native interfaces.
+#[derive(Debug, Clone, Parser)]
+#[command(about, long_about = include_str!("README.md"))]
+struct Args {
+    /// The function to generate cases for.
+    function: Function,
+    /// Does not need to be supplied, already exists in cargo environment.
+    #[arg(env)]
+    cargo_workspace_dir: std::path::PathBuf,
+}
+
+fn main() {
+    let Args {
+        function,
+        cargo_workspace_dir,
+    } = Args::parse();
+
+    let cases_dir = cargo_workspace_dir.join("native/backwards_compatibility_cases");
+
+    match function {
+        Function::ProcessInsert => {
+            process_insert::write_process_insert_cases(cases_dir);
+        }
+    }
+}
---
+++ native/src/bin/generate-backwards-compatibility-cases/write_cases.rs
@@ -0,0 +1,61 @@
+use std::path::Path;
+
+use codec::Encode;
+use proptest::prelude::*;
+use proptest::test_runner::{FileFailurePersistence, TestRng, TestRunner};
+use sp_core::keccak_256;
+
+/// Generates and writes cases to files.
+///
+/// Takes...
+/// - a proptest strategy to generate input
+/// - an `assert` function for callers to make assertions against input/output
+/// - the directory to write cases to
+pub fn write_cases<S, I, O, F, A, P>(strategy: S, f: F, assert: A, case_directory: P)
+where
+    S: Strategy<Value = I>,
+    F: Fn(I) -> O,
+    A: Fn(&I, &O) -> bool,
+    I: Encode + Clone + std::fmt::Debug,
+    O: Encode,
+    P: AsRef<Path> + Clone,
+{
+    let config = proptest::test_runner::Config {
+        cases: 64,
+        failure_persistence: Some(Box::new(FileFailurePersistence::Off)),
+        ..Default::default()
+    };
+
+    // using a deterministic RNG essentially gives the generation some idempotency, we won't
+    // clutter the cases directory with new cases unless some strategy has changed.
+    let rng = TestRng::deterministic_rng(config.rng_algorithm);
+
+    let mut runner = TestRunner::new_with_rng(config, rng);
+
+    runner
+        .run(&strategy, move |input| {
+            let input_bytes = input.encode();
+
+            let input_hash = keccak_256(&input_bytes);
+
+            let case_file = case_directory.as_ref().join(hex::encode(input_hash));
+
+            // skip cases we've already generated
+            if std::fs::exists(&case_file).unwrap() {
+                return Ok(());
+            }
+
+            let output = f(input.clone());
+
+            assert!(assert(&input, &output));
+
+            let case = (input, output);
+
+            let case_bytes = case.encode();
+
+            std::fs::write(&case_file, &case_bytes).unwrap();
+
+            Ok(())
+        })
+        .unwrap();
+}
