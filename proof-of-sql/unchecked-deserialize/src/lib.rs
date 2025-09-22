1551ba47c56ff77160f762c4259241bec831d223
Jay White <JayWhite2357@users.noreply.github.com>
2025-09-22 11:04:30 -0400

    feat: add `proof-of-sql-unchecked-deserialize` crate

---
+++ proof-of-sql/unchecked-deserialize/src/lib.rs
@@ -0,0 +1,6 @@
+//! This crate enables unchecked deserialization for certain types, in particular `TableCommitment<DynamicDoryCommitment>``.
+
+mod unchecked_dynamic_dory_commitment;
+pub use crate::unchecked_dynamic_dory_commitment::UncheckedDynamicDoryCommitment;
+mod table_commitment_util;
+pub use crate::table_commitment_util::map_table_commitment;
---
+++ proof-of-sql/unchecked-deserialize/src/table_commitment_util.rs
@@ -0,0 +1,19 @@
+//! Module containing utility functions `TableCommitment`s.
+
+use proof_of_sql::base::commitment::{Commitment, TableCommitment};
+
+/// Map a `TableCommitment<A>` to a `TableCommitment<B>` by applying `f` to each column commitment.
+pub fn map_table_commitment<'a, A: Commitment, B: Commitment, F: Fn(&'a A) -> B>(
+    table_commitment: &'a TableCommitment<A>,
+    f: F,
+) -> TableCommitment<B> {
+    TableCommitment::try_new(
+        table_commitment
+            .column_commitments()
+            .iter()
+            .map(|(i, m, c)| (i.clone(), *m, f(c)))
+            .collect(),
+        table_commitment.range().clone(),
+    )
+    .expect("range was cloned from existing commitment, so valid")
+}
