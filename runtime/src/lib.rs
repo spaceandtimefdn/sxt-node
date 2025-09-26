7dd3c1a56464396c7d3ad2a5c3f6a63243ba64e9
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-09-26 13:31:44 -0600

    perf: use new pallet-indexing weights to calculate fees
    The pallet-indexing weights have been recalculated to be more accurate.
    This change adjusts the fees of the network accordingly.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -366,7 +366,7 @@ pub const AVERAGE_INSERT_TARGET_COST: u128 = MILLICENTS
 pub const TARGET_BYTE_FEE: u128 =
     AVERAGE_INSERT_TARGET_COST.saturating_div(AVERAGE_INSERT_SIZE_BYTES);
 /// Approximated Average Insert Weight from actual transactions on testnet
-pub const AVERAGE_INSERT_CALL_WEIGHT: u128 = 65_686_228_000;
+pub const AVERAGE_INSERT_CALL_WEIGHT: u128 = 742_674_591_000;
 pub const WEIGHT_FEE: u128 = AVERAGE_INSERT_TARGET_COST.saturating_div(AVERAGE_INSERT_CALL_WEIGHT);
 
 parameter_types! {
