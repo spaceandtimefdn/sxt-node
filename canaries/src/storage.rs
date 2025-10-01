c90cf7e76322dc2a2ed9bf50e47f4efbb7a8df40
Ada Anderson <ada.anderson@makeinfinite.com>
2025-10-01 13:25:25 -0500

    feat: (Canary) break storage reads out to a new module

---
+++ canaries/src/storage.rs
@@ -0,0 +1,52 @@
+use anyhow::Result;
+use event_forwarder::chain_listener::{Block, API};
+use sxt_core::sxt_chain_runtime;
+
+/// Reads the active era, if one is present. Retuns Some(u32) if an era is found, None if there is
+/// no era, and an error if we were not able to read it.
+pub(crate) async fn read_active_era(block: &Block, api: &API) -> Result<Option<u32>> {
+    let active_era_query = sxt_chain_runtime::api::storage().staking().active_era();
+
+    if let Some(active_era) = api
+        .storage()
+        .at(block.hash())
+        .fetch(&active_era_query)
+        .await?
+    {
+        Ok(Some(active_era.index))
+    } else {
+        Ok(None)
+    }
+}
+
+pub(crate) async fn read_era_rewards(era: u32, block: &Block, api: &API) -> Result<Option<u128>> {
+    let era_reward_query = sxt_chain_runtime::api::storage()
+        .staking()
+        .eras_validator_reward(era);
+    if let Some(era_reward) = api
+        .storage()
+        .at(block.hash())
+        .fetch(&era_reward_query)
+        .await?
+    {
+        Ok(Some(era_reward))
+    } else {
+        Ok(None)
+    }
+}
+
+pub(crate) async fn read_total_staked(era: u32, block: &Block, api: &API) -> Result<Option<u128>> {
+    let total_staked_query = sxt_chain_runtime::api::storage()
+        .staking()
+        .eras_total_stake(era);
+    if let Some(total_staked) = api
+        .storage()
+        .at(block.hash())
+        .fetch(&total_staked_query)
+        .await?
+    {
+        Ok(Some(total_staked))
+    } else {
+        Ok(None)
+    }
+}
