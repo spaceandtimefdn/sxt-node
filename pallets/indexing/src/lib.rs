a6bc27f77eff21fdacd9bb226c9151d1b843ad94
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-12-01 15:57:32 -0700

    fix: use a different check for submissions v1 batch existence that doesn't have false positives

--- pallets/indexing/src/lib.rs
+++ pallets/indexing/src/lib.rs
@@ -440,6 +440,15 @@ pub mod pallet {
         T: Config<I>,
         I: NativeApi,
     {
+        // There is no `StorageNMap::contains_key_prefix`
+        if SubmissionsV1::<T, I>::iter_prefix((&batch_id,))
+            .next()
+            .is_none()
+        {
+            let batch_index = Pallet::<T, I>::batch_queue_bottom() + BatchQueue::<T, I>::count();
+            BatchQueue::<T, I>::insert(&batch_index, &batch_id);
+        }
+
         let submission_map_with_this =
             SubmissionsV1::<T, I>::iter_prefix((&batch_id, quorum_scope))
                 .take(MAX_SUBMITTERS as usize)
@@ -448,11 +457,6 @@ pub mod pallet {
 
         if submission_map_with_this.len() > MAX_SUBMITTERS as usize {
             Err(Error::MaxSubmittersReached::<T, I>)?;
-        }
-
-        if submission_map_with_this.len() == 1 {
-            let batch_index = Pallet::<T, I>::batch_queue_bottom() + BatchQueue::<T, I>::count();
-            BatchQueue::<T, I>::insert(&batch_index, &batch_id);
         }
 
         SubmissionsV1::<T, I>::insert((&batch_id, quorum_scope, &who), data_hash);
