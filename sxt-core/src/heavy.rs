dd3e735d172ad812997f01a4e08b445dc7333eea
Trevor Lovell <trevor.lovell@makeinfinite.com>
2025-12-01 17:00:20 -0700

    docs: add docs w/ doctests to Heavy module

--- sxt-core/src/heavy.rs
+++ sxt-core/src/heavy.rs
@@ -5,11 +5,30 @@ use frame_support::weights::Weight;
 /// A simple monad for composing functions that incur polkadot weight.
 #[derive(Copy, Clone, PartialEq, Eq, Debug, Default)]
 pub struct Heavy<T> {
+    /// The output of the operation.
     pub out: T,
+    /// The weight of the operation.
     pub weight: Weight,
 }
 
 impl<T> Heavy<T> {
+    /// Maps a `Heavy<T>` to a `Heavy<U>` by applying a function to the contained `out` field.
+    ///
+    /// # Examples
+    /// ```
+    /// use frame_support::weights::Weight;
+    /// use sxt_core::heavy::Heavy;
+    ///
+    /// let weight = Weight::zero().set_ref_time(1).set_proof_size(2);
+    ///
+    /// let initial = Heavy {
+    ///     out: 4,
+    ///     weight,
+    /// };
+    /// let mapped = initial.map(|n| n.to_string());
+    ///
+    /// assert_eq!(mapped, Heavy { out: "4".to_string(), weight });
+    /// ```
     pub fn map<U>(self, f: impl FnOnce(T) -> U) -> Heavy<U> {
         let Heavy { out, weight } = self;
         Heavy {
@@ -18,12 +37,60 @@ impl<T> Heavy<T> {
         }
     }
 
+    /// Apply a function `f` to the `out` and return a `Heavy` that sums the weights.
+    ///
+    /// `f` takes `out: T` and returns `Heavy<U>`. Then, this returns a `Heavy<U>` whose weight is
+    /// the sum of `self`'s weight and the weight returned by `f`.
+    ///
+    /// # Examples
+    /// ```
+    /// use frame_support::weights::Weight;
+    /// use sxt_core::heavy::Heavy;
+    ///
+    /// let weight = Weight::zero().set_ref_time(1).set_proof_size(2);
+    ///
+    /// let initial = Heavy {
+    ///     out: 4,
+    ///     weight,
+    /// };
+    /// let composed = initial.and_then(|n| {
+    ///     let weight = Weight::zero().set_ref_time(3).set_proof_size(4);
+    ///     Heavy { out: n.to_string(), weight }
+    /// });
+    ///
+    /// let expected_weight = Weight::zero().set_ref_time(4).set_proof_size(6);
+    ///
+    /// assert_eq!(composed, Heavy { out: "4".to_string(), weight: expected_weight });
+    /// ```
     pub fn and_then<U>(self, f: impl FnOnce(T) -> Heavy<U>) -> Heavy<U> {
         self.map(f).flatten()
     }
 }
 
 impl<T> Heavy<Heavy<T>> {
+    /// Flattens a nested `Heavy` by summing the weights.
+    ///
+    /// # Examples
+    /// ```
+    /// use frame_support::weights::Weight;
+    /// use sxt_core::heavy::Heavy;
+    ///
+    /// let inner_weight = Weight::zero().set_ref_time(1).set_proof_size(2);
+    /// let outer_weight = Weight::zero().set_ref_time(3).set_proof_size(4);
+    ///
+    /// let nested = Heavy {
+    ///     out: Heavy {
+    ///         out: 4,
+    ///         weight: inner_weight,
+    ///     },
+    ///     weight: outer_weight,
+    /// };
+    /// let flattened = nested.flatten();
+    ///
+    /// let expected_weight = Weight::zero().set_ref_time(4).set_proof_size(6);
+    ///
+    /// assert_eq!(flattened, Heavy { out: 4, weight: expected_weight });
+    /// ```
     pub fn flatten(self) -> Heavy<T> {
         let Heavy {
             out: Heavy {
