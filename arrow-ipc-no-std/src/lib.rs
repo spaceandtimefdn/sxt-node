b650d85d69ebca71034cc796cb6dc84ca47dca2f
Trevor Lovell <trevor.lovell@makeinfinite.com>
2026-03-04 18:23:30 +0000

    feat: add utility for parsing single-record-batch IPC stream
    Our data submission APIs accept record batch IPC bytes as input. More
    specifically, they accept single-batch arrow IPC streams as input.
    Parsing these streams into more logical types is slightly more
    complicated than deserializing flatbuffers.
    
    This change adds a utility for parsing single-record-batch IPC streams
    into the relevant flatbuffers types using a parser combinator pattern.

--- arrow-ipc-no-std/src/lib.rs
+++ arrow-ipc-no-std/src/lib.rs
@@ -1,5 +1,5 @@
 #![doc = include_str!("../README.md")]
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 #[expect(
     mismatched_lifetime_syntaxes,
@@ -15,3 +15,22 @@
 #[rustfmt::skip]
 #[path = "../target/flatbuffers/Message_generated.rs"]
 pub mod generated;
+
+mod stream_parser_combinators;
+pub use stream_parser_combinators::{
+    finish,
+    single_batch_stream_parser,
+    ArrowMessageParseError,
+    ArrowRecordBatchParseError,
+    ArrowSchemaParseError,
+    FinishParseError,
+    SingleBatchStream,
+    SingleBatchStreamParseError,
+};
+
+/// Included here for testing purposes, but in a way that is accessible to downstream that also
+/// need similar test/benchmark data.
+#[cfg(feature = "std")]
+mod write;
+#[cfg(feature = "std")]
+pub use write::single_batch_stream_bytes;
